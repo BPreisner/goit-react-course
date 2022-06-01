29c3fb1c2612d77076d80a339ab8f733dca341b5
Bartosz Preisner <bartosz.preisner@gmail.com>
2022-06-01 11:28:54 +0200

    feat(lesson-7): hooks introduction

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import { FaCartPlus } from 'react-icons/fa';
-import { Component } from 'react';
 import { productImageNotAvailableUrl } from './Product.utils';
 import {
   ProductWrapper,
@@ -12,51 +11,49 @@ import {
   ProductCard,
 } from './Product.styles';
 
-class Product extends Component {
-  static defaultProps = {
-    image: productImageNotAvailableUrl,
-  };
+const Product = ({
+  image = productImageNotAvailableUrl,
+  title,
+  price,
+  description,
+  onProductClick,
+}) => {
+  const amount = `${price ? price : 'Priceless :)'}`;
+  const currency = !!price && '$';
+  const isAddToCardActive = price > 0;
 
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    description: PropTypes.string,
-    image: PropTypes.string,
-    onProductClick: PropTypes.func.isRequired,
-  };
+  return (
+    <ProductCard shaded>
+      <ProductWrapper>
+        <ProductImage src={image} alt={title} />
+        <ProductInfo>
+          <h3>{title}</h3>
+          <Text>{description}</Text>
+          <Price>
+            {amount} {currency}
+          </Price>
+          <StyledButton
+            color="green"
+            appearance="primary"
+            disabled={!isAddToCardActive}
+            type="button"
+            onClick={onProductClick}
+          >
+            <FaCartPlus />
+            Add to cart
+          </StyledButton>
+        </ProductInfo>
+      </ProductWrapper>
+    </ProductCard>
+  );
+};
 
-  render() {
-    const { image, title, price, description, onProductClick } = this.props;
-
-    const amount = `${price ? price : 'Priceless :)'}`;
-    const currency = !!price && '$';
-    const isAddToCardActive = price > 0;
-
-    return (
-      <ProductCard shaded>
-        <ProductWrapper>
-          <ProductImage src={image} alt={title} />
-          <ProductInfo>
-            <h3>{title}</h3>
-            <Text>{description}</Text>
-            <Price>
-              {amount} {currency}
-            </Price>
-            <StyledButton
-              color="green"
-              appearance="primary"
-              disabled={!isAddToCardActive}
-              type="button"
-              onClick={onProductClick}
-            >
-              <FaCartPlus />
-              Add to cart
-            </StyledButton>
-          </ProductInfo>
-        </ProductWrapper>
-      </ProductCard>
-    );
-  }
-}
+Product.propTypes = {
+  title: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  description: PropTypes.string,
+  image: PropTypes.string,
+  onProductClick: PropTypes.func.isRequired,
+};
 
 export default Product;
